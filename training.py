e8cc3051ae974f928cd56fb388da6338f954bc65
duyenle1312 <81138129+duyenle1312@users.noreply.github.com>
2021-07-08 10:10:16 +0700

    Update saved files
    Save the tokenizer and label encoder to use in the testing.py

--- training.py
+++ training.py
@@ -63,4 +63,10 @@ history = model.fit(padded_sequences, np.array(training_labels), epochs=epochs)
 # to save the trained model
 model.save("chat_model.h5")
 
-# End of training
+# to save the fitted tokenizer
+with open('tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+# to save the fitted label encoder
+with open('label_encoder.pickle', 'wb') as ecn_file:
+    pickle.dump(lbl_encoder, ecn_file, protocol=pickle.HIGHEST_PROTOCOL)
